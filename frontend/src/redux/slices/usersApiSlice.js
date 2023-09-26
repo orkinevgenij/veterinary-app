4fa5f8d9eddb9abfc5a1c6fcef9c169d3e551a1f
evgenij <orkinevgenij@gmail.com>
2023-09-26 22:11:17 +0300

    first upload

--- frontend/src/redux/slices/usersApiSlice.js
+++ frontend/src/redux/slices/usersApiSlice.js
@@ -1,38 +1,38 @@
 import { apiSlice } from './apiSlice';
 
-const USERS_URL = 'api/users';
+const USERS_URL = 'https://veterinary-app.onrender.com/api/users';
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getMe: builder.query({
-      query: () => `/${USERS_URL}/profile`,
+      query: () => `${USERS_URL}/profile`,
       providesTags: ['User'],
     }),
     getAllUsers: builder.query({
-      query: (page) => `/${USERS_URL}/allusers?page=${page}`,
+      query: (page) => `${USERS_URL}/allusers?page=${page}`,
       providesTags: ['User'],
     }),
     getUserDetails: builder.query({
-      query: (userId) => `/${USERS_URL}/get-userdetails/${userId}`,
+      query: (userId) => `${USERS_URL}/get-userdetails/${userId}`,
       providesTags: ['User'],
     }),
     login: builder.mutation({
       query: (data) => ({
-        url: `/${USERS_URL}/auth`,
+        url: `${USERS_URL}/auth`,
         method: 'POST',
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        url: `/${USERS_URL}/`,
+        url: `${USERS_URL}/`,
         method: 'POST',
         body: data,
       }),
     }),
     createUser: builder.mutation({
       query: (data) => ({
-        url: `/${USERS_URL}/createuser`,
+        url: `${USERS_URL}/createuser`,
         method: 'POST',
         body: data,
       }),
@@ -40,7 +40,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
     }),
     updateProfile: builder.mutation({
       query: (data) => ({
-        url: `/${USERS_URL}/profile`,
+        url: `${USERS_URL}/profile`,
         method: 'PUT',
         body: data,
       }),
